346c8df625a581405cdfed625dc131a500db4ac2
vytautas <vytautas.rimeikis@gmail.com>
2018-07-04 09:27:43 +0300

    Article list

---
+++ database/migrations/2018_07_03_165628_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title', 255);
+            $table->text('decsription');
+            $table->string('author', 100);
+            $table->string('slug', 255)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
